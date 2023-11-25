73efedaa8aeb240780bc1814c5455dca04274aae
ryd3v <hello@ryd3v.com>
2023-11-25 16:53:08 -0500

    update scans

--- main.py
+++ main.py
@@ -29,6 +29,12 @@ import pexpect
 from tqdm import tqdm
 import os
 import sys
+import time
+import datetime
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import threading
+import itertools
 
 
 def check_tool_installed(tool_name):
@@ -47,6 +53,15 @@ def preflight_checks():
         check_tool_installed(tool)
 
 
+def show_progress_indicator(running):
+    symbols = itertools.cycle('[-\\|/]')
+    while running.is_set():
+        sys.stdout.write(next(symbols))
+        sys.stdout.flush()
+        time.sleep(0.1)
+        sys.stdout.write('\b')
+
+
 def scan_tcp_ports(ip, start_port, end_port=None, output_file=None):
     if end_port is None:
         end_port = 65535
@@ -58,11 +73,14 @@ def scan_tcp_ports(ip, start_port, end_port=None, output_file=None):
             port = futures[future]
             if future.result():
                 open_ports.append(port)
-            if output_file:
-                with open(output_file, 'w') as f:
-                    f.write("Open TCP ports on {}: {}\n".format(ip, open_ports))
-            else:
-                print("Open TCP ports on {}: {}".format(ip, open_ports))
+
+    if output_file:
+        with open(output_file, 'w') as f:
+            f.write("Open TCP ports on {}: {}\n".format(ip, open_ports))
+    else:
+        # Only print a summary if no output file is provided
+        print("Scan complete. Found {} open TCP ports on {}.".format(len(open_ports), ip))
+
     return open_ports
 
 
@@ -77,11 +95,14 @@ def scan_udp_ports(ip, start_port, end_port=None, output_file=None):
             port = futures[future]
             if future.result():
                 open_ports.append(port)
-            if output_file:
-                with open(output_file, 'w') as f:
-                    f.write("Open UDP ports on {}: {}\n".format(ip, open_ports))
-            else:
-                print("Open UDP ports on {}: {}".format(ip, open_ports))
+
+    if output_file:
+        with open(output_file, 'w') as f:
+            f.write("Open UDP ports on {}: {}\n".format(ip, open_ports))
+    else:
+        # Only print a summary if no output file is provided
+        print("Scan complete. Found {} open UDP ports on {}.".format(len(open_ports), ip))
+
     return open_ports
 
 
@@ -107,15 +128,28 @@ def scan_udp_port(ip, port):
 
 # Nikto
 def scan_with_nikto(url):
+    output_file = 'nikto.txt'
     try:
-        # Run the Nikto command and capture the output
-        command = f"nikto -host {url} -output='web.txt'"
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        for line in process.stdout:
-            print(line, end='')
-        process.wait()
+        command = f"nikto -host {url} -output='{output_file}'"
+
+        # Start progress indicator
+        running = threading.Event()
+        running.set()
+        progress_thread = threading.Thread(target=show_progress_indicator, args=(running,))
+        progress_thread.start()
+
+        subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+        # Stop progress indicator
+        running.clear()
+        progress_thread.join()
+
+        with open(output_file, 'r') as file:
+            nikto_results = file.read()
+        return nikto_results
     except subprocess.CalledProcessError as e:
         print(f"Error executing Nikto: {e.output}")
+        return None
 
 
 # Netdiscover
@@ -208,7 +242,32 @@ def run_nmap_scan(ip):
         print(f"Error executing nmap: {e.output}")
 
 
+def generate_pdf_report(file_name, scan_data, target, scan_type):
+    c = canvas.Canvas(file_name, pagesize=letter)
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Header
+    c.drawString(50, 800, "Ryd3v Toolkit Scan Report")
+    c.drawString(50, 785, f"Report Generated: {timestamp}")
+    c.drawString(50, 770, f"Target: {target}")
+    c.drawString(50, 755, f"Scan Type: {scan_type}")
+
+    # Body
+    y = 730
+    for line in scan_data.split('\n'):
+        c.drawString(50, y, line)
+        y -= 15
+        if y < 50:
+            c.showPage()
+            y = 750
+    c.save()
+
+
 def main():
+    tcp_scan_results = ""
+    udp_scan_results = ""
+    nikto_results = ""
+
     while True:
         print("Welcome to ryd3v Toolkit!")
         print("Select a task:")
@@ -219,7 +278,8 @@ def main():
         print("5. Web Directory Scanning (dirb)")
         print("6. Full TCP and UDP Port Scanning")
         print("7. Run nmap scan")
-        print("8. Exit")
+        print("8. Report Generation")
+        print("9. Exit")
         choice = int(input())
 
         if choice == 1:
@@ -227,22 +287,31 @@ def main():
             scan_option = input("Enter 'all' to scan all ports or 'range' to specify start and end ports: ")
             output_file = input("Enter the output file path (or leave it blank to display results on the console): ")
 
+            open_ports = []  # Initialize open_ports
+
             if scan_option.lower() == 'all':
-                open_ports = scan_tcp_ports(ip, start_port=1, end_port=65535, output_file=output_file)
+                open_ports = scan_tcp_ports(ip, start_port=1, end_port=65535)
             elif scan_option.lower() == 'range':
                 start_port = int(input("Enter the starting port: "))
                 end_port = int(input("Enter the ending port: "))
-                open_ports = scan_tcp_ports(ip, start_port=start_port, end_port=end_port, output_file=output_file)
+                open_ports = scan_tcp_ports(ip, start_port=start_port, end_port=end_port)
             else:
                 print("Invalid option. Please choose 'all' or 'range'.")
-                return
+                continue
 
-            print("Open TCP ports on {}: {}".format(ip, open_ports))
+            tcp_scan_results = "Open TCP ports on {}: {}".format(ip, open_ports)
+            print(tcp_scan_results)
+
+            # Write to file if the user has specified one
+            if output_file:
+                with open(output_file, 'w') as f:
+                    f.write(tcp_scan_results)
 
         elif choice == 2:
             ip = input("Enter the IP address to scan: ")
             scan_option = input("Enter 'all' to scan all ports or 'range' to specify start and end ports: ")
             output_file = input("Enter the output file path (or leave it blank to display results on the console): ")
+            open_ports = []  # Initialize open_ports
 
             if scan_option.lower() == 'all':
                 open_ports = scan_udp_ports(ip, start_port=1, end_port=65535, output_file=output_file)
@@ -252,13 +321,14 @@ def main():
                 open_ports = scan_udp_ports(ip, start_port=start_port, end_port=end_port, output_file=output_file)
             else:
                 print("Invalid option. Please choose 'all' or 'range'.")
-                return
+                continue
 
-            print("Open UDP ports on {}: {}".format(ip, open_ports))
+            udp_scan_results = "Open UDP ports on {}: {}".format(ip, open_ports)
+            print(udp_scan_results)
 
         elif choice == 3:
             url = input("Enter the URL you want to scan with Nikto: ")
-            scan_with_nikto(url)
+            nikto_results = scan_with_nikto(url)
 
         elif choice == 4:
             ip_range = input("Enter the IP address range (e.g., 192.168.2.1/24). "
@@ -282,7 +352,18 @@ def main():
             print("Nmap scan results saved to scan.txt.")
 
         elif choice == 8:
-            print("Exiting Cyber Swiss Army Knife Tool. Goodbye!")
+            if tcp_scan_results or udp_scan_results or nikto_results:
+                combined_results = f"{tcp_scan_results}\n{udp_scan_results}\n{nikto_results}"
+                report_file = "Ryd3v_Toolkit_Report.pdf"
+                target = "Your target information"  # Set this appropriately
+                scan_type = "Comprehensive Scan"
+                generate_pdf_report(report_file, combined_results, target, scan_type)
+                print(f"Report generated and saved as {report_file}")
+            else:
+                print("No scan results to report.")
+
+        elif choice == 9:
+            print("Exiting ryd3v Toolkit. Goodbye!")
             break
         else:
             print("Invalid option. Please choose a valid task.")
